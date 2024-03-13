4db3c3c89ae06b2000a736db4ed411c176422b89
Patricia Gameiro <patricia.a.gameiro@gmail.com>
2024-03-13 01:55:52 +0000

    rectangle is now filled

--- target.js
+++ target.js
@@ -8,7 +8,6 @@ class Target
     this.width  = w;
     this.label  = l;
     this.id     = id;
-    this.wasClicked = false;
   }
   
   // Getter
@@ -21,96 +20,74 @@ class Target
   // within the target
   clicked(mouse_x, mouse_y)
   { 
-    this.wasClicked = dist(this.x, this.y, mouse_x, mouse_y) < this.width / 2;
-    return this.wasClicked;
+    return dist(this.x, this.y, mouse_x, mouse_y) < this.width / 2;
   }
 
   // Draws the target and its label
   draw(colour, first, last)
-  {
+  { 
+    this.drawBox(colour, first, last);
+
     // Draw target
     fill(colour);                 
     circle(this.x, this.y, this.width);
-
-    if (!this.wasClicked)
-    {
-       // Draw inner circle (more contrast - black)
-      fill(color(0, 0, 0));
-      circle(this.x, this.y, this.width * 0.85);
-    }
-    else
-    {
-      // Draw inner circle (different color)
-      fill(color(60, 60, 60));
-      circle(this.x, this.y, this.width * 0.85);
-    }
-
+    fill(color(0, 0, 0));
+    circle(this.x, this.y, this.width * 0.9);
+ 
     // Draw label
-    textFont("Arial", 12);
+    textFont("Arial", 13);
     fill(color(255,255,255));
     textAlign(CENTER);
+
     // Capitalizing the third letter
-    let label = this.label[0] + this.label[1] + this.label[2].toUpperCase() + this.label.slice(3); 
+    let label = this.label[0] + this.label[1].toUpperCase() + this.label[2].toUpperCase() + this.label.slice(3); 
     text(label, this.x, this.y);
-
-
-    this.drawBox(colour, first, last);
-
-    noStroke();
 
     textFont("Arial", 16);
     fill(color(255, 255, 255));
   }
 
+  // Draws the box around the target
   drawBox(colour, first, last)
   {
-    strokeWeight(2);
-    noFill();
-    stroke(colour);
+    fill(color(colour));
 
     if (!first && !last) 
     {
-      //upper line
-      line(this.x - this.width - this.width/4, this.y - this.width/2, this.x + this.width + this.width/4, this.y - this.width/2);
-      // lower line
-      line(this.x - this.width - this.width/4, this.y + this.width/2, this.x + this.width + this.width/4, this.y + this.width/2);
+      rect(this.x, this.y - this.width/2, this.width * 2, this.width);
     }
 
-    else
+    if (first && !last)
+    { 
+      rect(this.x, this.y - this.width/2, this.width *2, this.width); 
+    }
+
+    if (last)
     {
-      // upper line
-      line(this.x - this.width * 3/4, this.y - this.width/2, this.x + this.width * 3/4, this.y - this.width/2);
-      // lower line
-      line(this.x - this.width * 3/4, this.y + this.width/2, this.x + this.width * 3/4, this.y + this.width/2);
+      fill(color(0, 0, 0));
+      rect(this.x, this.y - this.width/2, this.width, this.width);
+      fill(color(colour));
+    }
 
-      if (first) 
-      { 
-        // left line
-        line(this.x - this.width * 3/4, this.y - this.width/2, this.x -  this.width * 3/4, this.y + this.width/2);
-        noFill();
-        stroke(0); 
-        rect(this.x - this.width * 3/4, this.y - 18, 1, 36);
+    if (first && !last || first && last)
+    {
+      fill(color(0, 0, 0));
+      rect(this.x - this.width * 3/4, this.y - 18, 10, 40);
+      textFont("Cambria", 36);
+      fill(color(colour));
+      textAlign(CENTER);
     
-        textFont("Arial", 36);
-        fill(color(colour));
-        textAlign(CENTER);
-    
-        if (this.label[1] == 'é') 
-        {
-          text('e', this.x - this.width * 3/4, this.y + this.width/8);
-        }
-        else
-        {
-          text(this.label[1], this.x - this.width * 3/4, this.y + this.width/8);
-        }
+      if (this.label[1] == 'é') 
+      {
+        text('E', this.x - this.width * 3/4, this.y + this.width/8);
       }
-
-      if (last)
-      { 
-        // right line
-        line(this.x + this.width * 3/4, this.y - this.width/2, this.x +  this.width * 3/4, this.y + this.width/2);
+      else
+      {
+        text(this.label[1].toUpperCase(), this.x - this.width * 3/4, this.y + this.width/8);
       }
     }
+
+    noFill();
 
   }
 
